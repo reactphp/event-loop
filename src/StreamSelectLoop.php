reactphp/reactphp

Prototype of new loop that allows enabling and disabling listeners
This design has a major advantage: The definition of the listener and the
enabling are completely separate. Quite often you want to disable reads
as a pushback mechanism until your own buffer has space again. Similarly,
you only want to listen for writes when there is something to write.

Being able to enable and disable listeners without having to re-add the
actual listener callable is a lot more comfortable than having to juggle
the callbacks and re-add them all the time.

src/React/EventLoop/StreamSelectLoop.php
@@ -22,53 +22,65 @@ class StreamSelectLoop implements LoopInterface
         $this->timers = new Timers();
     }
 
-    public function addReadStream($stream, $listener)
+    public function onReadable($stream, callable $listener)
     {
         $id = (int) $stream;
 
-        if (!isset($this->readStreams[$id])) {
-            $this->readStreams[$id] = $stream;
-            $this->readListeners[$id] = $listener;
+        if (isset($this->readListeners[$id])) {
+            throw new \RuntimeException(sprintf('Stream %s already has a read listener.', $id));
         }
+
+        $this->readListeners[$id] = $listener;
     }
 
-    public function addWriteStream($stream, $listener)
+    public function enableRead($stream)
+    {
+        $id = (int) $stream;
+        $this->readStreams[$id] = $stream;
+    }
+
+    public function disableRead($stream)
+    {
+        $id = (int) $stream;
+        unset($this->readStreams[$id]);
+    }
+
+    public function onWritable($stream, callable $listener)
     {
         $id = (int) $stream;
 
-        if (!isset($this->writeStreams[$id])) {
-            $this->writeStreams[$id] = $stream;
-            $this->writeListeners[$id] = $listener;
+        if (isset($this->writeListeners[$id])) {
+            throw new \RuntimeException(sprintf('Stream %s already has a write listener.', $id));
         }
+
+        $this->writeListeners[$id] = $listener;
     }
 
-    public function removeReadStream($stream)
+    public function enableWrite($stream)
+    {
+        $id = (int) $stream;
+        $this->writeStreams[$id] = $stream;
+    }
+
+    public function disableWrite($stream)
+    {
+        $id = (int) $stream;
+        unset($this->writeStreams[$id]);
+    }
+
+    public function remove($stream)
     {
         $id = (int) $stream;
 
         unset(
             $this->readStreams[$id],
-            $this->readListeners[$id]
-        );
-    }
-
-    public function removeWriteStream($stream)
-    {
-        $id = (int) $stream;
-
-        unset(
+            $this->readListeners[$id],
             $this->writeStreams[$id],
             $this->writeListeners[$id]
         );
     }
 
-    public function removeStream($stream)
-    {
-        $this->removeReadStream($stream);
-        $this->removeWriteStream($stream);
-    }
-
-    public function addTimer($interval, $callback)
+    public function addTimer($interval, callable $callback)
     {
         $timer = new Timer($this, $interval, $callback, false);
         $this->timers->add($timer);
@@ -76,7 +88,7 @@ class StreamSelectLoop implements LoopInterface
         return $timer;
     }
 
-    public function addPeriodicTimer($interval, $callback)
+    public function addPeriodicTimer($interval, callable $callback)
     {
         $timer = new Timer($this, $interval, $callback, true);
         $this->timers->add($timer);
@@ -141,23 +153,27 @@ class StreamSelectLoop implements LoopInterface
         if (stream_select($read, $write, $except, 0, $timeout) > 0) {
             if ($read) {
                 foreach ($read as $stream) {
-                    if (!isset($this->readListeners[(int) $stream])) {
+                    $id = (int) $stream;
+
+                    if (!isset($this->readListeners[$id])) {
                         continue;
                     }
 
-                    $listener = $this->readListeners[(int) $stream];
-                    call_user_func($listener, $stream, $this);
+                    $listener = $this->readListeners[$id];
+                    $listener($stream, $this);
                 }
             }
 
             if ($write) {
                 foreach ($write as $stream) {
-                    if (!isset($this->writeListeners[(int) $stream])) {
+                    $id = (int) $stream;
+
+                    if (!isset($this->writeListeners[$id])) {
                         continue;
                     }
 
-                    $listener = $this->writeListeners[(int) $stream];
-                    call_user_func($listener, $stream, $this);
+                    $listener = $this->writeListeners[$id];
+                    $listener($stream, $this);
                 }
             }
         }

reactphp/event-loop

src/StreamSelectLoop.php
@@ -34,62 +34,80 @@ class StreamSelectLoop implements LoopInterface
     /**
      * {@inheritdoc}
      */
-    public function addReadStream($stream, callable $listener)
+    public function onReadable($stream, callable $listener)
     {
         $key = (int) $stream;
 
-        if (!isset($this->readStreams[$key])) {
-            $this->readStreams[$key] = $stream;
-            $this->readListeners[$key] = $listener;
+        if (isset($this->readListeners[$key])) {
+            throw new \RuntimeException(sprintf('Stream %s already has a read listener.', $key));
         }
+
+        $this->readListeners[$key] = $listener;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function addWriteStream($stream, callable $listener)
+    public function enableRead($stream)
     {
         $key = (int) $stream;
-
-        if (!isset($this->writeStreams[$key])) {
-            $this->writeStreams[$key] = $stream;
-            $this->writeListeners[$key] = $listener;
-        }
+        $this->readStreams[$key] = $stream;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function removeReadStream($stream)
+    public function disableRead($stream)
+    {
+        $key = (int) $stream;
+        unset($this->readStreams[$key]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onWritable($stream, callable $listener)
+    {
+        $key = (int) $stream;
+
+        if (isset($this->writeListeners[$key])) {
+            throw new \RuntimeException(sprintf('Stream %s already has a write listener.', $key));
+        }
+
+        $this->writeListeners[$key] = $listener;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enableWrite($stream)
+    {
+        $key = (int) $stream;
+        $this->writeStreams[$key] = $stream;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function disableWrite($stream)
+    {
+        $key = (int) $stream;
+        unset($this->writeStreams[$key]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove($stream)
     {
         $key = (int) $stream;
 
         unset(
             $this->readStreams[$key],
-            $this->readListeners[$key]
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function removeWriteStream($stream)
-    {
-        $key = (int) $stream;
-
-        unset(
+            $this->readListeners[$key],
             $this->writeStreams[$key],
             $this->writeListeners[$key]
         );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function removeStream($stream)
-    {
-        $this->removeReadStream($stream);
-        $this->removeWriteStream($stream);
     }
 
     /**