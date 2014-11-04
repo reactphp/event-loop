a643c7ab75118d17854cb98efee34d659b49ac3b
Steve Rhoades <sedonami@gmail.com>
2014-11-04 07:53:04 -0800

    updates based on code review

--- src/ExtEvLoop.php
+++ src/ExtEvLoop.php
@@ -12,7 +12,7 @@ class ExtEvLoop implements LoopInterface
 {
     private $loop;
     private $nextTickQueue;
-    private $futureTickQueue;    
+    private $futureTickQueue;
     private $timers;
     private $readEvents     = array();
     private $writeEvents    = array();
@@ -29,10 +29,7 @@ class ExtEvLoop implements LoopInterface
     }
 
     /**
-     * Add a readable stream to the event loop
-     * @param stream   $stream   PHP stream resource
-     * @param callable $listener Callback
-     * @return void
+     * {@inheritdoc}
      */
     public function addReadStream($stream, callable $listener)
     {
@@ -40,10 +37,7 @@ class ExtEvLoop implements LoopInterface
     }
 
     /**
-     * Add a writable stream to the event loop
-     * @param stream   $stream   PHP stream resource
-     * @param callable $listener Callback
-     * @return void
+     * {@inheritdoc}
      */
     public function addWriteStream($stream, callable $listener)
     {
@@ -51,62 +45,51 @@ class ExtEvLoop implements LoopInterface
     }
 
     /**
-     * Remove a readable stream from the event loop.
-     * @param  stream $stream PHP stream resource
-     * @return void
+     * {@inheritdoc}
      */
     public function removeReadStream($stream)
     {
         $key = (int) $stream;
-        if(isset($this->readEvents[$key])) {
-            $this->readEvents[$key]->stop();   
+        if (isset($this->readEvents[$key])) {
+            $this->readEvents[$key]->stop();
             unset($this->readEvents[$key]);
         }
     }
 
     /**
-     * remove a writable stream from the event loop
-     * @param  stream $stream PHP stream resource
-     * @return void         
+     * {@inheritdoc}
      */
     public function removeWriteStream($stream)
     {
         $key = (int) $stream;
-        if(isset($this->writeEvents[$key])) {
+        if (isset($this->writeEvents[$key])) {
             $this->writeEvents[$key]->stop();
             unset($this->writeEvents[$key]);
         }
     }
 
     /**
-     * remove a stream from the event loop
-     * @param  stream $stream PHP stream
-     * @return void         
+     * {@inheritdoc}
      */
     public function removeStream($stream)
     {
-        if (isset($this->readEvents[(int)$stream])) {
-            $this->removeReadStream($stream);
-        }
-
-        if (isset($this->writeEvents[(int)$stream])) {
-            $this->removeWriteStream($stream);
-        }
+        $this->removeReadStream($stream);
+        $this->removeWriteStream($stream);
     }
 
     /**
      * Wraps the listener in a callback which will pass the
      * stream to the listener then registers the stream with
      * the eventloop.
-     * 
-     * @param stream    $stream   PHP Stream resource
-     * @param callable  $listener stream callback
-     * @param bit       $flags    flag bitmask
+     *
+     * @param resource    $stream   PHP Stream resource
+     * @param callable    $listener stream callback
+     * @param int         $flags    flag bitmask
      */
     private function addStream($stream, callable $listener, $flags)
     {
         $listener = function ($event) use ($stream, $listener) {
-            call_user_func($listener, $stream);
+            call_user_func($listener, $stream, $this);
         };
 
         $event = $this->loop->io($stream, $flags, $listener);
@@ -151,13 +134,13 @@ class ExtEvLoop implements LoopInterface
 
             /* defer timer */
             $this->nextTick(function() use ($timer) {
-                $this->timers->detach($timer);    
+                $this->timers->detach($timer);
             });
         }
     }
 
     /**
-     * Add timer object as 
+     * Add timer object as
      * @param  TimerInterface $timer [description]
      * @return [type]                [description]
      */
@@ -180,6 +163,9 @@ class ExtEvLoop implements LoopInterface
         return $timer;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function isTimerActive(TimerInterface $timer)
     {
         return $this->timers->contains($timer);
@@ -202,7 +188,9 @@ class ExtEvLoop implements LoopInterface
         $this->futureTickQueue->add($listener);
     }
 
-
+    /**
+     * {@inheritdoc}
+     */
     public function tick()
     {
         $this->nextTickQueue->tick();
@@ -218,14 +206,21 @@ class ExtEvLoop implements LoopInterface
         $this->loop->run($flags);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function run()
     {
         $this->running = true;
+
         while($this->running) {
             $this->tick();
         }
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function stop()
     {
         $this->running = false;
@@ -234,15 +229,15 @@ class ExtEvLoop implements LoopInterface
     public function __destruct()
     {
         // mannually stop all watchers
-        foreach($this->timers as $timer) {
-            $this->timers[$timer]->stop();            
+        foreach ($this->timers as $timer) {
+            $this->timers[$timer]->stop();
         }
 
-        foreach($this->readEvents as $event) {
+        foreach ($this->readEvents as $event) {
             $event->stop();
         }
 
-        foreach($this->writeEvents as $event) {
+        foreach ($this->writeEvents as $event) {
             $event->stop();
         }
     }
