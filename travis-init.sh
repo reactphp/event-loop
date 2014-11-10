525d1cf644af5fb6ea84549c1535470d4e751aee
Steve Rhoades <sedonami@gmail.com>
2014-11-10 12:16:27 -0800

    try to install pecl ev with sudo

--- travis-init.sh
+++ travis-init.sh
@@ -12,7 +12,7 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
     echo "yes" | pecl install event
 
     # install 'ev' PHP extension
-    echo "yes" | pecl install ev
+    echo "yes" | sudo pecl install ev
 
     # install 'libevent' PHP extension
     curl http://pecl.php.net/get/libevent-0.1.0.tgz | tar -xz
@@ -23,6 +23,18 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
     make install
     popd
     echo "extension=libevent.so" >> "$(php -r 'echo php_ini_loaded_file();')"
+
+    # install 'pecl-ev' PHP extension
+    git clone http://bitbucket.org/osmanov/pecl-ev.git
+    # 0.2.10
+    pushd pecl-ev
+    phpize
+    ./configure
+    make
+    # make test
+    make install
+    popd
+    echo "extension=ev.so" >> "$(php -r 'echo php_ini_loaded_file();')"
 
     # install 'libev' PHP extension
     git clone --recursive https://github.com/m4rw3r/php-libev
