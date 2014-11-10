f01422d7b5eaae2451014ea8a6d3bd635b987d46
Steve Rhoades <sedonami@gmail.com>
2014-11-10 11:39:26 -0800

    now that segfault fix has been moved to pecl, use the ev pecl package

--- travis-init.sh
+++ travis-init.sh
@@ -11,6 +11,9 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
     # install 'event' PHP extension
     echo "yes" | pecl install event
 
+    # install 'ev' PHP extension
+    echo "yes" | pecl install ev
+
     # install 'libevent' PHP extension
     curl http://pecl.php.net/get/libevent-0.1.0.tgz | tar -xz
     pushd libevent-0.1.0
@@ -20,18 +23,6 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
     make install
     popd
     echo "extension=libevent.so" >> "$(php -r 'echo php_ini_loaded_file();')"
-
-    # install 'pecl-ev' PHP extension
-    git clone http://bitbucket.org/osmanov/pecl-ev.git
-    # 0.2.10
-    pushd pecl-ev
-    phpize
-    ./configure
-    make
-    # make test
-    make install
-    popd
-    echo "extension=ev.so" >> "$(php -r 'echo php_ini_loaded_file();')"
 
     # install 'libev' PHP extension
     git clone --recursive https://github.com/m4rw3r/php-libev
