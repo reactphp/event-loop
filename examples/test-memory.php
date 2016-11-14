27ce0787d042f6434c1ea61116f1d20702721eaf
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-11-14 13:29:12 +0100

    Applied argument suggestions by @clue from https://github.com/reactphp/event-loop/pull/59#issuecomment-260259139

--- examples/test-memory.php
+++ examples/test-memory.php
@@ -1,49 +1,55 @@
 <?php
 
 /**
- * Usage, runs indefinetly with the loop from Factory::create: php test-memory.php
- * Usage, runs for 6 seconds (anything lower than 5 seconds will be ignored): php test-memory.php 6
- * Usage, runs with the stream select loop for 6 seconds: php test-memory.php 6 StreamSelect
+ * Run the script indefinitely seconds with the loop from the factory and report every 2 seconds:
+ * php test-memory.php
+ * Run the script for 30 seconds with the stream_select loop and report every 10 seconds:
+ * php test-memory.php -t 30 -l StreamSelect -r 10
  */
 
 use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
 use React\EventLoop\Timer\TimerInterface;
 
 require __DIR__.'/vendor/autoload.php';
 
-if (isset($argv[2]) && class_exists('React\EventLoop\\' . $argv[2] . 'Loop')) {
-    $class = 'React\EventLoop\\' . $argv[2] . 'Loop';
-    $loop = new $class();
-} else {
-    $loop = Factory::create();
+$args = getopt('t:l:r:');
+$t  = isset($args['t']) ? (int)$args['t'] : 0;
+$loop = isset($args['l']) && class_exists('React\EventLoop\\' . $args['l'] . 'Loop') ? 'React\EventLoop\\' . $args['l'] . 'Loop' : Factory::create();
+
+if (!($loop instanceof LoopInterface)) {
+    $loop = new $loop();
 }
 
-$i = 0;
+$r = isset($args['r']) ? (int)$args['r'] : 2;
 
-if (isset($argv[1]) && 5 < (int)$argv[1]) {
-    $loop->addTimer((int)$argv[1], function (TimerInterface $timer) {
+$runs = 0;
+
+if (5 < $t) {
+    $loop->addTimer($t, function (TimerInterface $timer) {
         $timer->getLoop()->stop();
     });
 
 }
 
-$loop->addPeriodicTimer(0.001, function () use (&$i, $loop) {
-    $i++;
+$loop->addPeriodicTimer(0.001, function () use (&$runs, $loop) {
+    $runs++;
 
     $loop->addPeriodicTimer(1, function (TimerInterface $timer) {
         $timer->cancel();
     });
 });
 
-$loop->addPeriodicTimer(2, function () use (&$i) {
+$loop->addPeriodicTimer($r, function () use (&$runs) {
     $kmem = round(memory_get_usage() / 1024);
     $kmemReal = round(memory_get_usage(true) / 1024);
-    echo "Runs:\t\t\t$i\n";
+    echo "Runs:\t\t\t$runs\n";
     echo "Memory (internal):\t$kmem KiB\n";
     echo "Memory (real):\t\t$kmemReal KiB\n";
     echo str_repeat('-', 50), "\n";
 });
 
+echo "PHP Version:\t\t", phpversion(), "\n";
 echo "Loop\t\t\t", get_class($loop), "\n";
 echo "Time\t\t\t", date('r'), "\n";
 
@@ -54,7 +60,8 @@ $loop->run();
 $endTime = time();
 $timeTaken = $endTime - $beginTime;
 
+echo "PHP Version:\t\t", phpversion(), "\n";
 echo "Loop\t\t\t", get_class($loop), "\n";
 echo "Time\t\t\t", date('r'), "\n";
 echo "Time taken\t\t", $timeTaken, " seconds\n";
-echo "Runs per second\t\t", round($i / $timeTaken), "\n";
+echo "Runs per second\t\t", round($runs / $timeTaken), "\n";
