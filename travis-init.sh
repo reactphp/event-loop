79dd25c44d3675b5a52dd8f24f45ab058c8ef12c
Ivan Kalita <kaduev13@gmail.com>
2017-04-08 19:59:10 +0300

    Add PECL ev extension to travis-init.sh.

--- travis-init.sh
+++ travis-init.sh
@@ -8,6 +8,9 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" &&
     # install 'event' PHP extension
     echo "yes" | pecl install event
 
+    # install 'ev' PHP extension
+    echo "yes" | pecl install ev
+
     # install 'libevent' PHP extension (does not support php 7)
     if [[ "$TRAVIS_PHP_VERSION" != "7.0" &&
           "$TRAVIS_PHP_VERSION" != "7.1" ]]; then
