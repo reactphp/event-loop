ae621e441170725f010b4e86abc7c64222998cad
Christian Lück <christian@clue.engineering>
2022-02-04 09:16:59 +0100

    Improve bin tests to support running from dependency

--- tests/bin/12-undefined.php
+++ tests/bin/12-undefined.php
@@ -2,7 +2,8 @@
 
 use React\EventLoop\Loop;
 
-require __DIR__ . '/../../vendor/autoload.php';
+// autoload for local project development or project installed as dependency for reactphp/reactphp
+(@include __DIR__ . '/../../vendor/autoload.php') || require __DIR__ . '/../../../../autoload.php';
 
 Loop::get()->addTimer(10.0, function () {
     echo 'never';
