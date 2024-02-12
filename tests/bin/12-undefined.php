24172579f08b56eb791f9e3beb2213ce2aeafa35
Cees-Jan Kiewiet <ceesjank@gmail.com>
2024-02-12 20:53:12 +0100

    [WIP][3.x] Introduce PHPStan
    Full commit message will follow

--- tests/bin/12-undefined.php
+++ tests/bin/12-undefined.php
@@ -9,4 +9,9 @@ Loop::get()->addTimer(10.0, function () {
     echo 'never';
 });
 
+/**
+ * We're ignore this line because the test using this file relies on the error caused by it.
+ *
+ * @phpstan-ignore-next-line
+ */
 $undefined->foo('bar');
